9c00cf3bc3bb4575b64dbad8584cb7adcf03e461
Michael Ockenden <michael.ockenden@hotmail.com>
2021-09-02 20:13:03 +0100

    Backtesting improvements

--- backtester.py
+++ backtester.py
@@ -18,11 +18,25 @@ dates = []
 equity = []
 in_position = False
 
+trade_buy = []
+trade_sell = []
 
-with open("data/Bitfinex_BTCUSD_minute.csv", 'r') as file:
+count = 0
+
+# TODO: create backtesting folder and start separating logic
+# TODO: create risk manager/holdings manager, that can be used on backtesting and live trading
+# TODO: make it so not all equity is used on every order
+# TODO: add another asset (e.g. ETH)
+
+with open("data/Bitstamp_BTCUSD_2021_minute.csv", 'r') as file:
     reader = csv.DictReader(file)
 
     for line in reader:
+
+        count += 1
+        if count % 60 != 0:
+            continue
+
         price = float(line['close'])
         prices.append(price)
 
@@ -34,14 +48,14 @@ with open("data/Bitfinex_BTCUSD_minute.csv", 'r') as file:
         else:
             current_equity = START
 
-        if len(prices) > 100:
+        if len(prices) > 200:
             del prices[0]
 
         increase = (price - initial_price) / initial_price + 1
         price_movement.append(START * increase)
 
-        unix = int(line['unix']) / 1000
-        dates.append(datetime.fromtimestamp(unix))
+        date = datetime.fromtimestamp(int(line['unix']))
+        dates.append(date)
 
         action = analyse(prices)
 
@@ -53,11 +67,15 @@ with open("data/Bitfinex_BTCUSD_minute.csv", 'r') as file:
                 buy_at = price
                 saved_equity = current_equity
 
+                trade_buy.append((date, current_equity))
+
             elif action == SELL and in_position:
                 in_position = False
                 current_equity -= current_equity * TRADE_FEE
                 increase = (price - buy_at) / buy_at + 1
                 current_equity = saved_equity * increase
+
+                trade_sell.append((date, current_equity))
 
         elif in_position:
             increase = (price - buy_at) / buy_at + 1
@@ -68,7 +86,12 @@ with open("data/Bitfinex_BTCUSD_minute.csv", 'r') as file:
 x = numpy.array(dates)
 y1 = numpy.array(equity)
 y2 = numpy.array(price_movement)
+
 plt.plot(x, y1, label="Equity")
 plt.plot(x, y2, label="Buy and Hold")
 plt.legend()
+
+plt.scatter(*(zip(*trade_buy)), marker='v', c='red')
+plt.scatter(*(zip(*trade_sell)), marker='v', c='green')
+
 plt.show()
--- strategy.py
+++ strategy.py
@@ -1,12 +1,12 @@
 import numpy
 import talib
 
-RSI_PERIOD = 14
+RSI_PERIOD = 15
 RSI_OVERBOUGHT = 70
 RSI_OVERSOLD = 30
 
-EMA_SHORT = 28
-EMA_LONG = 89
+EMA_SHORT = 30
+EMA_LONG = 120
 
 BUY = 'buy'
 SELL = 'sell'
@@ -25,12 +25,12 @@ def analyse(closes=None):
     short_ema = talib.EMA(np_closes, EMA_SHORT)
     long_ema = talib.EMA(np_closes, EMA_LONG)
 
-    cross = short_ema[-1] - long_ema[-1]
+    cross = (short_ema[-1] - long_ema[-1]) / long_ema[-1] + 1
 
-    if latest_rsi * abs(cross) > RSI_OVERBOUGHT and cross < 0:
+    if latest_rsi / cross > RSI_OVERBOUGHT:
         return SELL
 
-    elif latest_rsi / abs(cross) < RSI_OVERSOLD and cross > 0:
+    elif latest_rsi / cross < RSI_OVERSOLD:
         return BUY
 
     else:
